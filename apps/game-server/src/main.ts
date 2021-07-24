e584cdcd71af014ac28ff76a019170d3a4a3f88b
Artem Baranov <temoncher@yandex.ru>
2021-07-24 14:42:46 +0300

    add strict eslint rules

--- apps/game-server/src/main.ts
+++ apps/game-server/src/main.ts
@@ -1,34 +1,34 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+import * as http from 'http';
 
 import * as express from 'express';
-import * as SocketIO from 'socket.io'
-import * as http from 'http';
+import * as SocketIO from 'socket.io';
 
 const PORT = process.env.port || 3333;
 
 const app = express();
 const server = http.createServer(app);
-const io = new SocketIO.Server(server, { cors: { origin: '*' }});
+const io = new SocketIO.Server(server, { cors: { origin: '*' } });
 
 app.get('/game', (req, res) => {
   res.send({ message: 'Welcome to game-server!' });
 });
 
 server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Listening at http://localhost:${PORT}/game`);
 });
 
+// eslint-disable-next-line no-console
 server.on('error', console.error);
 
 io.on('connection', (socket) => {
+  // eslint-disable-next-line no-console
   console.log('a user connected', socket.id);
 
   socket.on('greeting', () => {
+    // eslint-disable-next-line no-console
     console.log(`socket by id ${socket.id} greets us!`);
 
     socket.emit('greet-back');
-  })
+  });
 });
