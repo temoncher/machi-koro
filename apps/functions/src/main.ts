7b121744f3328bfdd4df302509f0a5785c278030
Artem Baranov <temoncher@yandex.ru>
2022-02-22 09:35:35 +0000

    change hello world function region to europe-west1

--- apps/functions/src/main.ts
+++ apps/functions/src/main.ts
@@ -3,7 +3,9 @@ import * as functions from 'firebase-functions';
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Firebase rocks!!');
-});
+export const helloWorld = functions
+  .region('europe-west1')
+  .https.onRequest((request, response) => {
+    functions.logger.info('Hello logs!', { structuredData: true });
+    response.send('Firebase rocks!!');
+  });
